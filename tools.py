af66cf4fbceb4d06485fdf7b25bd8d9f7fb77c4e
Tyler Neylon <tylerneylon@gmail.com>
2025-03-21 14:48:16 -0700

    Roll check_entry fnality into tools.py.

--- tools.py
+++ tools.py
@@ -9,7 +9,13 @@
 
         ./tools.py <command> [any command-specific options]
 
-    Here are all the commands available:
+    Here's a brief summary of all commands:
+
+        ./tools.py reversion [new-version-string]  # Update vers. in entries.
+        ./tools.py lookup [word]                   # Print `word`'s entry.
+
+    ______________________________________________________________________
+    Here are more details on each command:
 
         ./tools.py reversion [new-version-string]
 
@@ -24,6 +30,11 @@
       is no need to edit those files. Only entries.json needs to be updated.
     * You'll want to reversion the entries _before_ you save the results by
       running `evals.py html`.
+
+        ./tools.py lookup [word]
+
+    The above will look up the entry for `word` in entries.json and print out
+    the result.
 '''
 
 
@@ -32,6 +43,7 @@
 
 import json
 import sys
+from itertools import chain
 
 
 # ______________________________________________________________________
@@ -51,19 +63,26 @@ def load_entry_data(style='raw'):
     # TODO: Implement the 'indexed' version and use that elsewhere, including in
     #       check_entry.
 
+    # Basic input-checking.
+    assert style in ['raw', 'indexed']
+
     # Load in the AI-based entries.
     entries, redirects, errors = [], None, None
+    if style == 'indexed':
+        entries, redirects, errors = {}, {}, {}
     with open('entries.json') as f:
         for line in f:
             data = json.loads(line)
-            entries.append(data)
-            # word = data['word']
-            # if 'error' in data:
-            #     ai_errors[word] = data['error']
-            # elif 'base_word' in data:
-            #     ai
-            # else:
-            #     ai_data[word] = data['entry']
+            if style == 'raw':
+                entries.append(data)
+            elif style == 'indexed':
+                word = data['word']
+                if 'error' in data:
+                    errors[word] = data['error']
+                elif 'base_word' in data:
+                    redirects[word] = data['base_word']
+                else:
+                    entries[word] = data['entry']
     return entries, redirects, errors
 
 
@@ -83,6 +102,36 @@ def update_version_to(version_str):
             entry['version'] = version_str
             f.write(json.dumps(entry) + '\n')
     print('Done!')
+
+def did_find_cased_word(word, entries, redirects, errors):
+    # Try to find the word.
+    if word in entries:
+        print('Entry:')
+        print(json.dumps(entries[word], indent=4))
+        return True
+    elif word in redirects:
+        print(f'Redirect:\n  base_word={redirects[word]}')
+        return True
+    elif word in errors:
+        print('Error info:')
+        print(json.dumps(errors[word], indent=4))
+        return True
+    return False
+
+def lookup_word(word):
+    # Load the current data.
+    entries, redirects, errors = load_entry_data(style='indexed')
+
+    # Give first preference for the capitalization of the user.
+    if not did_find_cased_word(word, entries, redirects, errors):
+        # See if there's an adjusted capitalization we can find instead.
+        all_keys = chain(entries.keys(), redirects.keys(), errors.keys())
+        capitalization = {w: w.lower() for w in all_keys}
+        if word.lower() in capitalization:
+            word = capitalization[word.lower()]
+            did_find_cased_word(word, entries, redirects, errors)
+        else:
+            print(f'Couldn\'t find {word} in entries.json.')
 
 
 # ______________________________________________________________________
@@ -105,6 +154,10 @@ if __name__ == '__main__':
             print('Error: Please don\'t start your version string with v.')
             sys.exit(0)
         update_version_to(version_str)
+    elif sys.argv[1] == 'lookup':
+        if len(sys.argv) < 3:
+            print_docs_and_exit()
+        lookup_word(sys.argv[2])
     else:
         print(f'Unrecognized command: {sys.argv[1]}')
         sys.exit(0)
