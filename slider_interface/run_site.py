9463707df4adfceca93f7c59a19616a875d93b28
Tyler Neylon <tylerneylon@gmail.com>
2025-03-21 15:52:35 -0700

    Work on a slider interface for the landing page.

---
+++ slider_interface/run_site.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" run_site.py
+
+    This file illustrates a simple example of building a server with shotglass.
+
+    Run this as:
+
+        ./run_site.py          # to serve on port 80; or
+        ./run_site.py --debug  # to serve on port 8080, with verbose output.
+
+    See the docstring in shotglass.py for more details.
+"""
+
+
+# _______________________________________________________________________
+# Imports
+
+import shotglass
+
+
+# _______________________________________________________________________
+# Handler functions
+
+# These functions receive the following data:
+#  * URL path parameters, in the order given, and
+#  * The raw data string (not yet parsed), *only if* it's a POST request.
+
+def get_hello_html():
+    return b'<html><body><h1>hello</h1></body></html>'
+
+
+# _______________________________________________________________________
+# Main
+
+if __name__ == '__main__':
+
+    # The format here is:
+    # [<path>, <handler_fn>, <list_of_keywords>]; the last value is optional
+    # and indicates which keyword arguments the handler function accepts.
+    # The handler fn is called as fn(*path_args, [data, ] **kwargs); the
+    # path_args are the $-delimited path words, data exists iff it's a POST,
+    # and kwargs are the url query parameters listed in `optional_keywords`.
+
+    GET_routes = [
+            # ['/', get_hello_html]
+            # ['/a/$b$/c', my_handler_fn, ['optional_keyword']]
+    ]
+
+    POST_routes = [
+    ]
+
+    # Uncomment the next line to require basic authentication.
+    # shotglass.set_basic_auth(True, 'user', 'pass')
+
+    shotglass.register_routes(GET_routes, POST_routes)
+    shotglass.add_static_paths([
+        'index.html',
+        'words.json'
+    ])
+
+    shotglass.run_server()
